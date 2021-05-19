5f4214230c69f426fb7b619458c0c82ad7f7823e
Ricardo <ricardinhogiasson16@gmail.com>
2021-05-19 09:27:26 -0300

    Save SVG Categories

--- src/modules/cars/useCases/ImportCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/ImportCategory/ImportCategoryUseCase.ts
@@ -10,7 +10,7 @@ interface IImportCategory {
 export class ImportCategoryUseCase {
   constructor(private categoriesRepository: CategoryRepository) {}
 
-  async loadCategories(file: Express.Multer.File):Promise<IImportCategory[]> {
+  async loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
     return new Promise((resolve, reject) => {
       const categories: IImportCategory[] = [];
       const stream = fs.createReadStream(file.path);
@@ -18,22 +18,28 @@ export class ImportCategoryUseCase {
       stream.pipe(parseFile);
 
       parseFile
-        .on("data", async(line) => {
+        .on("data", async (line) => {
           const [name, description] = line;
           categories.push({ name, description });
         })
         .on("end", () => {
           resolve(categories);
-        }).on("error", (err)=>{
-            reject(err);
         })
-
-      
+        .on("error", (err) => {
+          reject(err);
+        });
     });
   }
 
   async execute(file: Express.Multer.File): Promise<void> {
     const categories = await this.loadCategories(file);
-    console.log(categories);
+    categories.map((category) => {
+      const { name, description } = category;
+
+      const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+      if (!categoryAlreadyExists) {
+        this.categoriesRepository.create({ name, description });
+      }
+    });
   }
 }
